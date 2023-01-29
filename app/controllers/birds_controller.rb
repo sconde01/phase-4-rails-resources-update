cc8c968549cd8f29a992731a825025aa96d4fbc7
Stephanie Conde <stephaniecondecareer@gmail.com>
2023-01-28 21:29:12 -0800

    commit tree daf4ad68823e9af3287e7bff25cb5e07c419a40e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,32 @@ class BirdsController < ApplicationController
     end
   end
 
+  #PATCH /birds/:id
+  # def update
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     bird.update(bird_params)
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
+  #PATCH using custom route--this changes our RESTful API behavio. One alternate way to structure this kind of feature and keep our routes and controllers RESTful would be to create a new controller, such as Birds::LikesController, and add an update action in this controller. 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
